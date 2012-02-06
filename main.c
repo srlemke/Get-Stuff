d2a9b4a1d9624927f2e447ef0a76f54a8a13175e
Sergio Rafael Lemke <bedi.com@gmail.com>
2012-02-06 14:41:02 -0200

    Removed GTK crap and using a old, good and working terminal interface.

--- main.c
+++ main.c
@@ -1,171 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <gtk/gtk.h>
 #include "common.h"
 
-//Taskbar icon//
-GdkPixbuf *create_pixbuf(const gchar * filename)
+void menu()
 {
-	GdkPixbuf *pixbuf;
-	GError *error = NULL;
-	pixbuf = gdk_pixbuf_new_from_file(filename, &error);
-	if(!pixbuf) {
-		fprintf(stderr, "%s\n", error->message);
-		g_error_free(error);
-	}
-	return pixbuf;
-}
-
-int main( int argc, char **argv)
-{	
-	gtk_init(&argc, &argv);
-
-	int skype_status, flash_status, codecs_status;
-
-	if(getuid()!=0)
-	{
-		GtkWidget *janela, *dialog;
-
-		janela = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-		gtk_window_set_icon(GTK_WINDOW(janela), create_pixbuf("/usr/share/icons/mandriva.png"));
-
-		dialog = gtk_message_dialog_new(GTK_WINDOW(janela),
-				GTK_DIALOG_DESTROY_WITH_PARENT,
-				GTK_MESSAGE_WARNING,
-				GTK_BUTTONS_OK,
-				"Please, run this as root user.");
-		gtk_window_set_title(GTK_WINDOW(dialog), "Warning:");
-		gtk_dialog_run(GTK_DIALOG(dialog));
-		gtk_widget_destroy(dialog);
+	printf ("Insert a number to: \n");
+	if(skype()){
+		printf("1 - Uninstall Skype\n");
 	}else{
-		GtkWidget *janela, *label, *skype_button_label;
-		GtkWidget *choice_box;
-		GtkWidget *extras0, *skype_button;
-		GtkWidget *extras1, *flash_button;
-		GtkWidget *extras2, *codecs_button;
-		GtkWidget *extras3, *quit_button, *valign;
-
-		///// The main window ////////////
-		janela = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-		gtk_window_set_position(GTK_WINDOW(janela), GTK_WIN_POS_CENTER);
-
-		////// Adds icon to systemtray and window-decoration/////////
-		gtk_window_set_icon(GTK_WINDOW(janela), create_pixbuf("/usr/share/icons/mandriva.png"));
-
-		////// Define the type of box for this variables
-		choice_box = gtk_vbox_new(TRUE, 6);
-		extras0 = gtk_hbox_new(TRUE, 6);
-		extras1 = gtk_hbox_new(TRUE, 6);
-		extras2 = gtk_hbox_new(TRUE, 6);
-		extras3 = gtk_hbox_new(TRUE, 6);
-		label = gtk_label_new("Select an option or press Quit");
-		skype_button_label =  gtk_label_new("mimimimimimim");
-		valign = gtk_alignment_new(6, 6, 6, 6);
-
-		////////////////////////////////////////
-
-		///Window Title////////////
-		gtk_window_set_title(GTK_WINDOW(janela), "Mandriva 2011 Stuff Installer");
-		///////////////
-
-		/////// Adds label to choice_box and choice_box do main_window ///////
-		gtk_container_add(GTK_CONTAINER(choice_box), label);
-		gtk_container_add(GTK_CONTAINER(janela), choice_box);
-
-		/////// Adds horizontal boxes with buttons to the choice_box //////
-		gtk_container_add(GTK_CONTAINER(choice_box), extras0);
-		gtk_container_add(GTK_CONTAINER(choice_box), extras1);
-		gtk_container_add(GTK_CONTAINER(choice_box), extras2);
-		gtk_container_add(GTK_CONTAINER(extras3), valign);
-		gtk_container_add(GTK_CONTAINER(choice_box), extras3);
-
-		///// Create the buttons/////////////////
-
-		//install_skype_button = gtk_button_new_with_label("Install Skype");
-		//gtk_box_pack_start (GTK_BOX (extras0), install_skype_button, 0, 1, 20);
-
-		//Skype gtk buttons//
-		if(!skype()){
-			skype_button = gtk_button_new_with_label("Install skype!");
-			skype_status = 0;
-		}else{
-			skype_button = gtk_button_new_with_label("Uninstall skype!");
-			skype_status = 1;
-		}
-		gtk_box_pack_start (GTK_BOX (extras0), skype_button, 6, 6, 6);
-		////////////////
-		
-		//flash gtk buttons
-		if(flash()){
-			flash_button = gtk_button_new_with_label("uninstall flash!");
-			flash_status = 0;
-		}else{
-			flash_button = gtk_button_new_with_label("install flash!");
-			flash_status = 1;
-		}
-		gtk_box_pack_start (GTK_BOX (extras1), flash_button, 6, 6, 6);
-		////////////////
-
-		if(TRUE){
-			codecs_button = gtk_button_new_with_label("uninstall codecs!");
-			codecs_status = 0;
-		}else{
-			codecs_button = gtk_button_new_with_label("uninstall codecs!");
-			codecs_status = 1;
-		}
-		gtk_box_pack_start (GTK_BOX (extras2), codecs_button, 6, 6, 6);
-
-		quit_button = gtk_button_new_with_label("Close");
-		gtk_box_pack_start (GTK_BOX (extras3), quit_button, 6, 6, 6);
-
-		///// end of button creation///////////
-
-		//////// Show the master_window and all its babies//////
-		gtk_widget_show_all(janela);
-		////////////
-
-		//////////// Funtions to buttons when they are clicked///////////
-		if(skype_status == 0){
-			(g_signal_connect(skype_button, "clicked",
-					  G_CALLBACK(install_skype), NULL));
-				gtk_label_set_text(GTK_LABEL(skype_button), "Uninstall Skype");
-		}else{
-			(g_signal_connect(skype_button, "clicked",
-					  G_CALLBACK(uninstall_skype), NULL));
-				gtk_label_set_text(GTK_LABEL(skype_button), "Install Skype");
-		}
-		
-		if(flash_status == 1){
-			(g_signal_connect(flash_button, "clicked",
-					  G_CALLBACK(install_flash), NULL));
-				gtk_label_set_text(GTK_LABEL(flash_button), "Uninstall Flash");
-		}else{
-			(g_signal_connect(flash_button, "clicked",
-					  G_CALLBACK(uninstall_flash), NULL));
-				gtk_label_set_text(GTK_LABEL(flash_button), "Install Flash");
-		}
-		
-		if(codecs_status == 1){
-			(g_signal_connect(flash_button, "clicked",
-					  G_CALLBACK(install_flash), NULL));
-				gtk_label_set_text(GTK_LABEL(codecs_button), "Uninstall Codecs");
-		}else{
-			(g_signal_connect(flash_button, "clicked",
-					  G_CALLBACK(uninstall_flash), NULL));
-				gtk_label_set_text(GTK_LABEL(codecs_button), "Uninstall Codecs");
-		}
-
-		g_signal_connect(quit_button, "clicked",
-				G_CALLBACK(gtk_main_quit), G_OBJECT(janela));
-		//////////////////////////////////////////////////////////////
-
-		////The main window(eg. kwin) close button ///////////////////
-		g_signal_connect_swapped(G_OBJECT(janela), "destroy",
-				G_CALLBACK(gtk_main_quit), NULL);
-		////////////////////////////////////////////////////////////////
-
-		gtk_main();
+		printf("1 - Install Skype\n");
 	}
-	return 0;
+	
+	if(flash()){
+		printf("2 - Uninstall Flash\n");
+	}else{
+		printf("2 - Install Flash\n");
+	}
 }
+
+int main()
+{
+	int choose = 0;
+	menu();
+
+	scanf("%d", &choose);
+	if(choose != 1 || choose != 2){
+		printf ("Invalid number\n");
+		menu();
+	}
+
+	if(choose == 1){
+		if(skype()){
+			uninstall_skype();
+		}else{
+			install_skype();
+		}
+	}
+	
+	if(choose == 2){
+		if(flash()){
+			uninstall_flash();
+		}else{
+			install_flash();
+		}
+	}
+	return 1;
+}
+
